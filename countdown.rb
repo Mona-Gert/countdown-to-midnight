a5ac4256cb4043ebb6e8e12e727f45e493156930
monamohamed <mona.m.mohamed@gmail.com>
2020-12-09 13:32:52 -0500

    commit tree 87cfb2c712624af8e8de454a4b60fcc6bb893a98

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown (number)
+    while number > 0
+        puts "#{number} SECOND(S)!"
+        number -= 1
+    end
+    return "HAPPY NEW YEAR!"
 end
+def countdown_with_sleep(x)
+    while x > 0
+    puts "#{x} SECOND(S)!"
+    sleep 1
+    x -= 1
+  end
+  return "HAPPY NEW YEAR!"
+  end
+  
